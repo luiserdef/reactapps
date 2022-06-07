656d93ff8cb594d36a31527916876872678db9f1
Luis Sánchez <sanchezluis9820@gmail.com>
2022-06-06 23:48:43 -0500

    base files

---
+++ codecampreplic/src/App.js
@@ -0,0 +1,16 @@
+
+import Testimony from './components/Testimony.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimony/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ codecampreplic/src/components/Testimony.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Testimony.css'
+
+
+function Testimony(){
+    return(
+            <div className='contenedor-testimonio'>
+                <img 
+                className='imagen-testimonio'
+                src={require('../img/testimony-emma.png')}
+                alt='Emma-photography'
+                />
+                <div className='contenedor-texto-testimonio'>
+                    <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
+                    <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
+                    <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+                </div>   
+            </div>
+    );
+}
+
+export default Testimony
