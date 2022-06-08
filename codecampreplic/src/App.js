2c7ad9aa26cdfd284f16eb1e83c87e060bceb4b6
Luis Sánchez <sanchezluis9820@gmail.com>
2022-06-07 22:53:20 -0500

    first app complete

--- codecampreplic/src/App.js
+++ codecampreplic/src/App.js
@@ -1,5 +1,5 @@
-
 import Testimony from './components/Testimony.js'
+import { testimonyData } from './data/testimony-data.js';
 import './App.css';
 
 function App() {
@@ -7,7 +7,20 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimony/>
+
+        {testimonyData.map((testimony,index) => {
+            return(
+              <Testimony key={index}
+              nombre={testimony.nombre}
+              pais={testimony.pais}
+              imagen={testimony.imagen}
+              cargo={testimony.cargo}
+              empresa={testimony.empresa}
+              testimonio={testimony.testimonio}
+            />
+            )
+            console.log(testimony)
+         })}
       </div>
     </div>
   );
--- codecampreplic/src/components/Testimony.js
+++ codecampreplic/src/components/Testimony.js
@@ -2,18 +2,18 @@ import React from 'react';
 import '../styles/Testimony.css'
 
 
-function Testimony(){
+function Testimony(props){
     return(
             <div className='contenedor-testimonio'>
                 <img 
                 className='imagen-testimonio'
-                src={require('../img/testimony-emma.png')}
+                src={require(`../img/testimony-${props.imagen}.png`)}
                 alt='Emma-photography'
                 />
                 <div className='contenedor-texto-testimonio'>
-                    <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-                    <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-                    <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+                    <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+                    <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+                    <p className='texto-testimonio'>"{props.testimonio}"</p>
                 </div>   
             </div>
     );
