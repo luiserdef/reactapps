9ed44d49cdff3a2dc2ac1fc3b100901694835b5b
Luis Sánchez <sanchezluis9820@gmail.com>
2022-06-16 23:21:34 -0500

    task list- main complete

--- taskslists/src/components/Tarea.js
+++ taskslists/src/components/Tarea.js
@@ -2,13 +2,14 @@ import React from "react";
 import  '../styles/Tarea.css'
 import {AiOutlineCloseCircle} from 'react-icons/ai'
 
-function Tarea({texto, completada}){
+function Tarea({id,texto, completada, completarTarea, eliminarTarea}){
     return(
         <div className={ completada ? 'tarea-contenedor completada' : 'tarea-contenedor'} >
-            <div className='tarea-texto'>
+            <div onClick={()=>completarTarea(id)} className='tarea-texto'>
                 {texto}
             </div>
-            <div className='tarea-contenedor-iconos'>
+            <div className='tarea-contenedor-iconos'
+            onClick={()=>eliminarTarea(id)}>
                 <AiOutlineCloseCircle className='tarea-icono'/>
             </div>
         </div>
